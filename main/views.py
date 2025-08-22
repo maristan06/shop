74bbadc79e093d0a99535421af53d6608fc96e0f
maristan06 <harutumann@icloud.com>
2025-08-22 13:26:37 +0200

    worked on the visual part

--- main/views.py
+++ main/views.py
@@ -5,12 +5,25 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     context: dict = {
-        'title': 'Home',
-        'content': 'main page HOME',
+        'title': 'Aleteia Jewelry homepage',
+        'content': 'Aleteia Jewelry store',
     }
 
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse('about page')
+    context: dict = {
+        'title': 'about us',
+        'content': 'About Aleteia Jewelry',
+        'text_on_page': """Eye-catching jewelry
+            that is hand-crafted from the highest quality
+            Japanese glass beads and hypoallergenic stainless steel findings,
+            something you will enjoy wearing for a stroll in the city,
+            to the office, on a date or a special event -
+            the variety of Aleteia creations has no limit!
+            Bead by bead with care and attention to detail
+            we create jewelry specially for YOU!"""
+    }
+
+    return render(request, 'main/about.html', context)

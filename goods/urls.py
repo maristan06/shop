a54f3bf0798c570be84630fcdbf3f493189d6432
maristan06 <harutumann@icloud.com>
2025-08-29 11:32:44 +0200

    added pagination

--- goods/urls.py
+++ goods/urls.py
@@ -6,5 +6,4 @@ app_name = 'goods'
 urlpatterns = [
     path('<slug:category_slug>/', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
-
-]
+    ]

e6adb4003dd773f350dcbd787a2ab139bf6ca9c2
maristan06 <harutumann@icloud.com>
2025-09-10 12:29:21 +0200

    registration complete

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -27,8 +27,18 @@ def login(request):
 
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('user:login'))
+    else:
+        form = UserRegistrationForm()
     context = {
         'title': 'registration',
+        'form': form,
     }
     return render(request, 'users/registration.html', context)
 
@@ -41,7 +51,5 @@ def profile(request):
 
 
 def logout(request):
-    context = {
-        'title': 'authorization',
-    }
-    return render(request, '', context)
+    auth.logout(request)
+    return redirect(reverse('main:index'))
